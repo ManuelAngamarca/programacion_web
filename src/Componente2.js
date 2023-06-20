794f05b587c768e20ae9aa09d68ed8796a566644
ManuelAngamarca <manuel.g.angamarca@unl.edu.ec>
2023-06-19 23:31:22 -0500

    Se agrega el mensaje en el main

---
+++ src/Componente2.js
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Componente2(props){
+    return(
+        <div>
+            <h2>
+            Hello
+            </h2>
+        </div>
+    );
+}
